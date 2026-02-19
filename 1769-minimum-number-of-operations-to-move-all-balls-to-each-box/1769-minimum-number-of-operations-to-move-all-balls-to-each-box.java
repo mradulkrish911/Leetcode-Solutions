c104246cf5cc09988bc877a82998e6c75fc108fd
Mradul Krishna Bhardwaj <mradulkrish911@gmail.com>
2026-02-19 10:40:46 +0530

    Time: 131 ms (25.15%), Space: 46.6 MB (52.16%) - LeetHub

---
+++ 1769-minimum-number-of-operations-to-move-all-balls-to-each-box/1769-minimum-number-of-operations-to-move-all-balls-to-each-box.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int[] minOperations(String boxes) {
+        int arr[] = new int[boxes.length()];
+
+        for(int i = 0 ; i < boxes.length(); i++){
+            int sum = 0;
+
+            for(int j = 0; j < boxes.length(); j++){
+                if (boxes.charAt(j) == '1') {
+                    sum += Math.abs(i - j);
+                }
+            }
+            arr[i] = sum;          
+        }
+        return arr;                
+    }
+}
