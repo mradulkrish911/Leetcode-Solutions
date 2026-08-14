6387a794398b1ecf7e04dd06dc54d6160e78387c
Mradul Krishna Bhardwaj <mradulkrish911@gmail.com>
2026-08-14 10:02:40 +0530

    Time: 1 ms (86.42%), Space: 44.3 MB (20.52%) - LeetHub

--- 0078-subsets/0078-subsets.java
+++ 0078-subsets/0078-subsets.java
@@ -1,20 +1,32 @@
 class Solution {
+
+    void func(List<Integer> nums, List<Integer> temp, List<List<Integer>> ans) {
+        ans.add(new ArrayList<>(temp));
+
+        if (nums.size() == 0) return;
+
+        for (int i = 0; i < nums.size(); i++) {
+            List<Integer> ip = new ArrayList<>(nums);
+            List<Integer> op = new ArrayList<>(temp);
+
+            op.add(nums.get(i));
+            ip.subList(0, i + 1).clear();
+
+            func(ip, op, ans);
+        }
+    }
+
     public List<List<Integer>> subsets(int[] nums) {
-
-        List<List<Integer>> li = new ArrayList<>();
-
-        for(int mask = 0; mask < (1 << nums.length); mask++ ){
-            List<Integer> line = new ArrayList<>();
-
-            for(int i = 0; i < nums.length ; i++){
-                if((mask & (1 << i)) != 0){
-                    line.add(nums[i]);
-                }
-            }
-            li.add(line);
+        List<Integer> input = new ArrayList<>();
+        for (int num : nums) {
+            input.add(num);
         }
 
-        return li;
-        
+        List<Integer> temp = new ArrayList<>();
+        List<List<Integer>> ans = new ArrayList<>();
+
+        func(input, temp, ans);
+
+        return ans;
     }
 }
