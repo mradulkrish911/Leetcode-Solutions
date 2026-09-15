f6b7898b3e513feb36cf9ed64b9c0499ab4fbac0
Mradul Krishna Bhardwaj <mradulkrish911@gmail.com>
2026-09-15 19:15:14 +0530

    Time: 88 ms (40.21%), Space: 47.1 MB (14.06%) - LeetHub

--- 0494-target-sum/0494-target-sum.java
+++ 0494-target-sum/0494-target-sum.java
@@ -1,20 +1,28 @@
 class Solution {
     public int findTargetSumWays(int[] nums, int target) {
-        return fun(nums, target, 0);
+        HashMap<String, Integer> dp = new HashMap<>();
+        return fun(nums, target, 0, dp);
     }
 
-    int fun(int[] nums, int target, int i) {
+    int fun(int[] nums, int target, int i, HashMap<String, Integer> dp) {
 
         if (i >= nums.length) {
             return target == 0 ? 1 : 0;
         }
 
+        String key = i + "," + target;
+
+        if (dp.containsKey(key))
+            return dp.get(key);
+
         // Add
-        int take = fun(nums, target - nums[i], i + 1);
+        int take = fun(nums, target - nums[i], i + 1, dp);
 
         // Subtract
-        int notTake = fun(nums, target + nums[i], i + 1);
+        int not_take = fun(nums, target + nums[i], i + 1, dp);
 
-        return take + notTake;
+        dp.put(key, take + not_take);
+
+        return take + not_take;
     }
 }
